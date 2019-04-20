50897fd8f4d9c5383e3b4b0e2d10a02cbb539d08
InspurKelly <13636814471@163.com>
2019-04-20 20:48:11 +0800

    2019/4/20

--- src/test/java/com/nesc/utilites/JoinerTest.java
+++ src/test/java/com/nesc/utilites/JoinerTest.java
@@ -1,6 +1,7 @@
 package com.nesc.utilites;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 import org.junit.Test;
 
@@ -9,6 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -29,7 +31,7 @@ public class JoinerTest {
             "google","guava", null, "", "kafka"
     );
 
-    private final String tagetFileName = "D:\\ideawork\\guava-study\\file\\guava-joiner.txt";
+    private final String targetFileName = "D:\\ideawork\\guava-study\\file\\guava-joiner.txt";
 
     @Test
     public void testJoinOnJoin() {
@@ -73,9 +75,9 @@ public class JoinerTest {
     @Test
     public void testJoin_On_Append_To_Writer() {
 
-        try (FileWriter writer = new FileWriter(new File(tagetFileName))) {
+        try (FileWriter writer = new FileWriter(new File(targetFileName))) {
             Joiner.on("#").useForNull("default").appendTo(writer, stringListWithNullValue);
-            assertThat(Files.isFile().test(new File(tagetFileName)), equalTo(true));
+            assertThat(Files.isFile().test(new File(targetFileName)), equalTo(true));
         } catch (IOException e) {
             fail("append to the writer occur fetal error.");
             e.printStackTrace();
@@ -87,4 +89,37 @@ public class JoinerTest {
         String result = stringListWithNullValue.stream().filter(item -> null != item && !item.isEmpty()).collect(Collectors.joining("#"));
         assertThat(result, equalTo("google#guava#scala#kafka"));
     }
+
+    @Test
+    public void testJoiningByStreamWithDeafultValue() {
+        /*
+            map 传入function 返回function
+         */
+        String result = stringListWithNullValue.stream().map(this::defaultValue).collect(Collectors.joining("#"));
+        assertThat(result, equalTo("google#guava#default#scala#kafka"));
+    }
+
+    private String defaultValue(final String item) {
+        return null == item || item.isEmpty() ? "default" : item;
+    }
+
+    private final Map<String, String> stringMap = ImmutableMap.of("Hello", "Guava", "Java", "Scala");
+
+    private final String targetFileNameToMap = "D:\\ideawork\\guava-study\\file\\guava-joiner-map.txt";
+
+    @Test
+    public void testJoinOnWithMap() {
+        assertThat(Joiner.on('#').withKeyValueSeparator("=").join(stringMap), equalTo("Hello=Guava#Java=Scala"));
+    }
+
+    @Test
+    public void testJoinOnWithMapToAppendable() {
+        try (FileWriter writer = new FileWriter(new File(targetFileNameToMap))){
+            Joiner.on("#").withKeyValueSeparator("=").appendTo(writer, stringMap);
+            assertThat(Files.isFile().test(new File(targetFileNameToMap)), equalTo(true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
